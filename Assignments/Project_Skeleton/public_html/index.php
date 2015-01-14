73675ffece5151c5fe7931fb8ce15deaa1d6d2ef
unknown <vpb04020@CLK310-02.vtc.vsc.edu>
2015-01-13 19:43:06 -0500

    hello world lab

---
+++ Assignments/Project_Skeleton/public_html/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+
+<html>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Changa+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Special+Elite' rel='stylesheet' type='text/css'>
+<meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="thewebsiteex.css" media="screen"/>
+<link rel="stylesheet" type="text/css" href="thewebsiteprint.css" media="print"/>
+
+</head>
+
+<body>
+<?php
+	echo "Hello World!";
+	?>
+<div id="gold"><center><h1>W o r l d&nbsp&nbsp o f &nbsp&nbspM u</h1>
+<p>Home of the Namaland fantasy series.</p></center></div>
+<img id="croc" src="crocodile2IV.png" width="100%">
+
+<nav><ul>
+<li><a href=shortstorypage.html> story page&nbsp&nbsp</a></li>
+<li><a href=buythebookpage.html>Buy the Book Page&nbsp&nbsp</a></li>
+<li><a href=Authorsblog.html&nbsp>Author's blog; write or die.</a></li>
+
+</ul></nav>
+
+
+
+<div class="column-1L">
+In our future ruins a barbarian will awaken to see his tribe has vanished, a Naga princess shall slither away from her home, an elf will stand against his own people to defend another, a crocodile being will spend his life hating humans only to be saved by one, and a mysterious farm girl shall be the one who rescues him.  These five outcast will unite, to build civilization anew. Yet odds will stack against them, for they must face down savages, beast, their own peoples, and the dark, primordial force that once brought collapse to all of civilization.    
+
+<embed width="420" height="30" src="http://www.youtube.com/v/wpWzGKDj2cE">
+</div>
+
+<div class="column-2r">
+<center><p>Chapter 2</p>
+<p><pre>Five gods would found our civilization
+The second looked deep into the earth
+And found eternity in the stone.
+He observed the plans for our civilization
+and then carved it out of the mountains
+Behold Akol.
+Our god of sculpting and the past.</pre></p></center>
+
+	<p>Akol the crocodile pounded stakes deep into a slab of granite, his hulking muscles allowing him to do the work of four humans. Soon a block would be split from a slab. As he worked the reptile grumbled and hissed when he recalled bumping into Arevelion an hour earlier. Dam elves, He thought. They're nothing, but humans with pointed ears and fancy tools. The chief will learn the hard way.</p>
+	<p>The mighty reptile blew his nose into the air. Anything to get the dust out of his nostrils. Then he started to cough. His lungs hacked so violently it caused his sculpted and rippled body to collapse in a fit of wheezing. He needed to build a fire before the cold came. He amassed whatever dry sticks he could on the bare mountain side, but he was at war with his treacherous lungs, which coughed him to the ground again and again. Whenever Akol was brought down hacking and wheezing he would force his fist into the ground and push himself back up. Yet as the cold blanket of night brought dew on to the grass, it also sapped his strength, his energy and his life. His lungs brought him to the ground just as he was about to rub two sticks together, and the night prevented Akol from getting up again.  Like any other reptile in the chill, Akol became a stiff, tired and vulnerable heap. His slit eyes could focus on nothing, but sighted a blurry figure approaching him up against a starry and black background.</p>
+	<p>The crocodile did not even have the strength to lift his hammer in his defense, but he did have life in him to say one thing.</p>
+	<p>“Come to carve me up huh, human?” he said as his eye lids became heavy. “That's alright, I ain't too young to die.”  Akol's eye lids collapsed from exhaustion, and he felt pressure on his back.</p>
+	<p><i>Must be the knife carving me up,</i> he thought, for he realized he was too anesthetized to feel much pain. <i>So does my life flash before my eyes or what?</i>  The outside world disappeared for the reptile as he passed out and started to dream.  As Akol peered into the nothingness he suddenly found himself back in a city where a human boy begged him for coin.</p>
+	<p>“Please,” he said, “my mother and I are starving.” He held out his bony hands.</p>
+	<p>“Huh! Even the king can't make something of you humans,” Akol recalled himself saying.</p>
+	<p><i>Why is it of all the things I could remember when I die, I see this? I have a whole life that could flash!</i> Akol thought to himself. He recalled batting the child away with his muscular tail, and then showing the child the hard, plated, scales that armored his back. He thought, <i>I could have been nicer to that kid, even if he was a human.</i></p>
+	<p>Nothing is constant in a dream. Many a dream shift without reason and this one did. Suddenly Akol heard the child's voice change in such a way to shock him.</p>  
+	<p>“I'm sorry, Dad,” the figure said. “But I have to go help people.” Without logic the dream had turned the human boy into a young crocodile, a crocodile that was Akol's son. As Akol's son jumped into the black void Akol screamed, “ Asheer, Nooooo!” He tried to grab his son, but couldn't because Asheer was just a transient soul. </p>
+	<p>Immediately Akol saw the white light above him.
+	<i>Is that supposed to be heaven? It's not like I was good enough to go there.</i> He thought to himself. Then he came to a sudden realization. <i>I don't believe it, it's the sun! I'm alive!</i> 
+	<p>The living reptile's eyes then swivelled to see the ashes of the fires that were lit next to him, and his ear picked up the sound of snoring, and he felt something warm and soft on his back.
+	Akol then realized, That human wasn't carving me up. It was building a fire and sleeping on top of me to keep me warm. He gently tilted his body, and when the human slid of his back he caught her gently with his tail. The human was a woman whose body was emaciated and bony. Akol also saw her feet were calloused, worn, and scarred.  On her head a few gray hairs split and splayed from what otherwise seemed to be a youthful face.</p>
+	<p>The woman's eye lids opened gradually and when they did they looked into Akol's. The woman opened her mouth as though to say something, but after several contortions she failed to utter anything but a pained groan. Akol slid his massive claws under the woman, and lifted her into his arms, supporting her back and cupping her head like she were a child.
+	Even in Akol's arms the woman decided to make one last go at speaking. She hauled her heavy arm to her chest, placed her hand between sunken breast, and uttered, “Cava.”</p>
+	<p>“You're gonna live, Cava,” Akol responded. “You're gonna live.”</p></div>
+
+</body>
+
+</html>
